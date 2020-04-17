dd5af27736cd2cbbeee67ec01f04ffa7c56e8d77
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 19:51:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each {|name| puts(name)}
+  end
+
+end
+
+
+
+class Song
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
