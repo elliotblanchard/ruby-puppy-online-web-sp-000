02011096a61bc82d66b7999080cff10b8ae3f6f3
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 19:57:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,13 +4,13 @@ class Dog
 
   attr_accessor :name
 
+  def self.save
+    @@all << self
+  end
+
   def initialize(name)
     @name = name
     self.save
-  end
-
-  def self.save
-    @@all << self
   end
 
   def self.all
